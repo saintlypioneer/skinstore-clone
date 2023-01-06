ff36fb3ccd595d216693c7cdf3e30f9f782b8533
Ankit Agrawal <aggrawalankitsra@gmail.com>
2023-01-06 11:20:20 +0530

    crete template for global JS file and add specific product page

---
+++ global.js
@@ -0,0 +1,20 @@
+const items = [
+    // {id: 1, ...}
+];
+
+function addToCart(id){
+    // this id is from items
+    // localStorage key name => 'cart'
+}
+
+function getFilteredProducts(category){
+    // filter the product by category from items and display using render function
+}
+
+function updateBreadcrums(){
+
+}
+
+function updateQuantity(id, typeOfOperation){
+
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+const items = [
+    // {id: 1, ...}
+];
+
+function addToCart(id){
+    // this id is from items
+    // localStorage key name => 'cart
+}
