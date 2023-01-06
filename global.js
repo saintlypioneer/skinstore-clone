d70b2df9e8b68d9ba78bb8b33e845bf1224de183
AkashSaha747 <akashkalyaganj5@gmail.com>
2023-01-06 20:22:01 +0530

    done with the js logic of login and sign up page

--- global.js
+++ global.js
@@ -1,214 +1,338 @@
 const items = [
-   //Skincare products
-   {
+  //Skincare products
+  {
     id: 1,
-    image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/11401314-1954461013882644.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original/productimg/1600/1600/11401314-1954461013882644.jpg",
     name: "3LAB Anti-Ageing Cream 60ml",
     category: "Skincare",
-    price: 750.00,
- },
+    price: 750.0,
+  },
 
- {
+  {
     id: 2,
-    image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/12038576-2264648083937035.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original/productimg/1600/1600/12038576-2264648083937035.jpg",
     name: "Omorovicza Best Sellers Set",
     category: "Skincare",
-    price: 480.00,
- },
+    price: 480.0,
+  },
 
- {
+  {
     id: 3,
-    image_url: "https://static.thcdn.com/images/small/webp//productimg/1600/1600/12538011-5734765050447840.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/webp//productimg/1600/1600/12538011-5734765050447840.jpg",
     name: "Omorovicza Evening CBD Facial Set",
     category: "Skincare",
-    price: 402.00,
- },
+    price: 402.0,
+  },
 
- {
+  {
     id: 4,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13928075-1205002137017610.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/13928075-1205002137017610.jpg",
     name: " Zutta LED Mask - Sapphira",
     category: "Skincare",
-    price: 399.00,
- },
+    price: 399.0,
+  },
 
- //Hair products
- {
+  //Hair products
+  {
     id: 5,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13280908-8024900284917718.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/13280908-8024900284917718.jpg",
     name: " Unplugged Styler - Cordless Flat Iron (Black)",
     category: " Hair Care",
-    price: 299.00,
- },
+    price: 299.0,
+  },
 
- {
+  {
     id: 6,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13554654-1074930173695055.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/13554654-1074930173695055.jpg",
     name: " T3 Afar Travel Dryer",
     category: "Hair Care",
     price: 179.99,
- },
+  },
 
- {
+  {
     id: 7,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/11785994-1984985603593023.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/11785994-1984985603593023.jpg",
     name: " Philip B Icelandic Blonde Shampoo 32 fl oz/947ml",
     category: "Hair Care",
-    price: 150.00,
- },
+    price: 150.0,
+  },
 
- {
+  {
     id: 8,
-    image_url: "https://static.thcdn.com/images/small/webp//productimg/original/13282502-3944891492637181.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/webp//productimg/original/13282502-3944891492637181.jpg",
     name: " VIRTUE Healthy Scalp Trio",
     category: "Hair Care",
-    price: 134.00,
- },
+    price: 134.0,
+  },
 
- //makeup & cosmetics
- {
+  //makeup & cosmetics
+  {
     id: 9,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13937560-1154998594986205.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/13937560-1154998594986205.jpg",
     name: " Chantecaille Oil Free Balancing Moisturizer 50g",
     category: "Makeup and Cosmetics",
-    price: 75.00,
- },
+    price: 75.0,
+  },
 
- {
+  {
     id: 10,
-    image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/11567391-1644522663544346.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original/productimg/1600/1600/11567391-1644522663544346.jpg",
     name: " Dermablend Natural Finish Set (Various Shades)",
     category: "Makeup and Cosmetics",
-    price: 64.00,
- },
+    price: 64.0,
+  },
 
- {
+  {
     id: 11,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/11348400-1224871519735586.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/11348400-1224871519735586.jpg",
     name: " Colorescience Natural Finish Pressed Foundation SPF20 (Various Shades)",
     category: "Makeup and Cosmetics",
-    price: 59.00,
- },
+    price: 59.0,
+  },
 
- {
+  {
     id: 12,
-    image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11752239-8694597305372915.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11752239-8694597305372915.jpg",
     name: "Koh Gen Do Maifanshi Moisture Fit Concealer 7g (Various Shades)",
     category: "Makeup and Cosmetics",
-    price: 63.00,
- },
+    price: 63.0,
+  },
 
- {
+  {
     id: 13,
-    image_url: "https://static.thcdn.com/images/small/webp//productimg/original/11065895-5134853691666783.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/webp//productimg/original/11065895-5134853691666783.jpg",
     name: "NUDESTIX Mascara - Black",
     category: "Makeup and Cosmetics",
-    price: 28.00,
- },
+    price: 28.0,
+  },
 
- {
+  {
     id: 14,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/11378683-1514991779261836.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/11378683-1514991779261836.jpg",
     name: "Illamasqua Antimatter Lipstick (Various Shades)",
     category: "Makeup and Cosmetics",
-    price: 11.00,
- },
+    price: 11.0,
+  },
 
- {
+  {
     id: 15,
-    image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11742829-1834590785162913.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11742829-1834590785162913.jpg",
     name: "TONYMOLY Petit Bunny Gloss Bars (Option: 02 | Juicy Grape)",
     category: "Makeup and Cosmetics",
-    price: 8.00,
- },
+    price: 8.0,
+  },
 
- {
+  {
     id: 16,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/12657561-1764964030368092.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/12657561-1764964030368092.jpg",
     name: "Stila Sketch and Sculpt Brow Pencil 4g (Various Shades)",
     category: "Makeup and Cosmetics",
-    price: 20.00,
- },
+    price: 20.0,
+  },
 
- //Bath & Body
- {
+  //Bath & Body
+  {
     id: 17,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/12507566-5965014350757716.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/12507566-5965014350757716.jpg",
     name: " Mama Mio Supersize Bundle",
     category: "Bath & Body",
-    price: 91.00,
- },
+    price: 91.0,
+  },
 
- {
+  {
     id: 18,
-    image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11725414-2914576050842040.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11725414-2914576050842040.jpg",
     name: "Elemis Love Yourself Bundle",
     category: "Bath & Body",
-    price: 79.60,
- },
+    price: 79.6,
+  },
 
- {
+  {
     id: 19,
-    image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11691581-4674555612912272.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11691581-4674555612912272.jpg",
     name: "Elemis Body Performance Bundle",
     category: "Bath & Body",
-    price: 108.50,
- },
+    price: 108.5,
+  },
 
- {
+  {
     id: 20,
-    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13030216-1194867945123005.jpg",
+    image_url:
+      "https://static.thcdn.com/images/small/original//productimg/original/13030216-1194867945123005.jpg",
     name: "Soon Skincare Clear Day Broad Spectrum SPF50 2 oz",
     category: "Bath & Body",
-    price: 48.00,
- }
+    price: 48.0,
+  },
 ];
 
-function addToCart(id){
-    // this id is from items
-    // localStorage key name => 'cart'
+function addToCart(id) {
+  // this id is from items
+  // localStorage key name => 'cart'
 }
 
-function getFilteredProducts(category){
-    // filter the product by category from items and display using render function
+function getFilteredProducts(category) {
+  // filter the product by category from items and display using render function
 }
 
-function updateBreadcrums(){
-
-}
-
-function updateQuantity(id, typeOfOperation){
-
-}
-
-function onLoadLanding(){
-
-}
-
-function onLoadProductPage(){
-
-}
-
-function onLoadSpecificProductPage(){
-
-}
-
-function onLoadCheckoutPage(){
-
-}
-
-function onLoadSigninPage(){
+function updateBreadcrums() {
     
 }
 
-function onSignIn(){
-
-}
-
-function onSignUp(){
+function updateQuantity(id, typeOfOperation) {
     
 }
 
-function onLoadSignupPage(){
+function onLoadLanding() {
     
+}
+
+function onLoadProductPage() {
+    
+}
+
+function onLoadSpecificProductPage() {
+    
+}
+
+function onLoadCheckoutPage() {
+    
+}
+
+function onLoadSigninPage() {
+    
+}
+
+function onSignIn() {
+  var signInArr = JSON.parse(localStorage.getItem("signUpUserDetails")) || [];
+  // select the signin button as below--
+  // var signInBtn=document.querySelector(".signin_div>div >div:nth-child(1) >input:nth-child(1)");
+
+  var email = document.querySelector(
+    ".signin_div .child1 div:nth-child(2) input"
+  ).value;
+  var pass = document.querySelector(
+    ".signin_div >.parent >.child1 div:nth-child(3) input"
+  ).value;
+
+  // console.log(email,pass);
+  if (email == "" || pass == "") {
+    alert("Fill all inputs");
+  } else {
+    var flag = 0;
+    signInArr.map(function (ele) {
+      if (ele.pass == pass && ele.email == email) {
+        flag = 1;
+      }
+    });
+    if (flag == 1) {
+      alert("SIgn In Sucessful");
+      window.location.href = "index.html";
+    } else {
+      alert("Please enter valid email or password");
+    }
+  }
+}
+
+function onSignUp() {
+  var signUpUserDetailsArr =
+    JSON.parse(localStorage.getItem("signUpUserDetails")) || [];
+  const doc = document.getElementsByClassName("signup_div");
+  var name = document.querySelector(".signup_div >div .inp1 input ");
+  name = name.value;
+
+  var email = document.querySelector(".signup_div >div .inp2 input ");
+  email = email.value;
+
+  var email_confirm = document.querySelector(".signup_div >div .inp3 input ");
+  email_confirm = email_confirm.value;
+
+  var pass = document.querySelector(".signup_div >div .inp4 input ");
+  pass = pass.value;
+
+  var pass_confirm = document.querySelector(".signup_div >div .inp5 input ");
+  pass_confirm = pass_confirm.value;
+
+  var phone = document.querySelector(".signup_div >div .inp6 input ");
+  phone = phone.value;
+  // console.log(name,email,email_confirm,pass,pass_confirm,phone);
+  if (
+    name == "" ||
+    email == "" ||
+    email_confirm == "" ||
+    pass == "" ||
+    pass_confirm == ""
+  ) {
+    alert("Please fill all the required inputs");
+  } else if (
+    !email.includes("@gmail.com") &&
+    !email.includes("@rediffmail.com")
+  ) {
+    alert("Enter a Valid Gmail or Rediffmail");
+  } else if (email != email_confirm) {
+    alert("Missmatch in Email");
+  } else if (pass.length < 8) {
+    alert("Use Atleast 8 characters for password");
+  } else if (pass != pass_confirm) {
+    alert("Missmatch in Password");
+  } else if (phone.length != 10 && phone.length >= 1) {
+    alert("Enter Valid Phone Number");
+  } else {
+    alert("SIGN UP SUCCESSFUL");
+    var obj = {
+      name: name,
+      email: email,
+      pass: pass,
+    };
+    // console.log(obj);
+    signUpUserDetailsArr.push(obj);
+    localStorage.setItem(
+      "signUpUserDetails",
+      JSON.stringify(signUpUserDetailsArr)
+    );
+    window.location.href = "./index.html";
+  }
+
+  //not taking phone number as it is optional in web page **
+}
+
+function onLoadSignupPage() {
+
+}
+
+
+var passwordDiv = document.querySelector(" input[type='password']");
+if (passwordDiv != null) {
+  passwordDiv.parentNode.children[1].addEventListener("click", showPassFunc);
+  console.log(passwordDiv.parentNode.children[1]);
+
+  function showPassFunc() {
+    // console.log(passwordDiv.children[0]);
+    passwordDiv.parentNode.children[0].setAttribute("type", "text");
+    passwordDiv.parentNode.children[1].innerHTML = "hide";
+    passwordDiv.parentNode.children[1].addEventListener("click", hidePassFunc);
+  }
+  function hidePassFunc() {
+    passwordDiv.parentNode.children[0].setAttribute("type", "password");
+    passwordDiv.parentNode.children[1].innerHTML = "show";
+    passwordDiv.parentNode.children[1].addEventListener("click", showPassFunc);
+  }
 }
