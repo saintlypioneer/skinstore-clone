74ae7dff562c6d9882a035484d7ff79dd803cfb0
Ankit Agrawal <aggrawalankitsra@gmail.com>
2023-01-06 11:33:59 +0530

    added some product info to js file

--- global.js
+++ global.js
@@ -1,5 +1,31 @@
 const items = [
-    // {id: 1, ...}
+    { id:1,
+        image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/11401314-1954461013882644.jpg",
+        name: "3LAB Anti-Ageing Cream 60ml" ,
+        category: "Skincare",
+        price: 750.00,
+     },
+     
+     { id:2,
+         image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/12038576-2264648083937035.jpg",
+         name:"Omorovicza Best Sellers Set" ,
+         category: "Skincare",
+         price: 480.00,
+      },
+     
+      { id:3,
+         image_url: "https://static.thcdn.com/images/small/webp//productimg/1600/1600/12538011-5734765050447840.jpg",
+         name: "Omorovicza Evening CBD Facial Set" ,
+         category: "Skincare",
+         price: 402.00,
+      },
+     
+      { id:4,
+         image_url: "https://static.thcdn.com/images/small/original//productimg/original/13928075-1205002137017610.jpg",
+         name: " Zutta LED Mask - Sapphira" ,
+         category: "Skincare",
+         price: 399.00,
+      }
 ];
 
 function addToCart(id){
