4b87d7e1d84b38a8650731d9ad23da9ed5f0c65d
AkashSaha747 <akashkalyaganj5@gmail.com>
2023-01-06 16:07:32 +0530

    fixed issues with global js file

--- global.js
+++ global.js
@@ -1,167 +1,167 @@
 const items = [
-        //Skincare products
-        {
-           id: 1,
-           image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/11401314-1954461013882644.jpg",
-           name: "3LAB Anti-Ageing Cream 60ml",
-           category: "Skincare",
-           price: 750.00,
-        },
-     
-        {
-           id: 2,
-           image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/12038576-2264648083937035.jpg",
-           name: "Omorovicza Best Sellers Set",
-           category: "Skincare",
-           price: 480.00,
-        },
-     
-        {
-           id: 3,
-           image_url: "https://static.thcdn.com/images/small/webp//productimg/1600/1600/12538011-5734765050447840.jpg",
-           name: "Omorovicza Evening CBD Facial Set",
-           category: "Skincare",
-           price: 402.00,
-        },
-     
-        {
-           id: 4,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/13928075-1205002137017610.jpg",
-           name: " Zutta LED Mask - Sapphira",
-           category: "Skincare",
-           price: 399.00,
-        },
-     
-        //Hair products
-        {
-           id: 5,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/13280908-8024900284917718.jpg",
-           name: " Unplugged Styler - Cordless Flat Iron (Black)",
-           category: " Hair Care",
-           price: 299.00,
-        },
-     
-        {
-           id: 6,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/13554654-1074930173695055.jpg",
-           name: " T3 Afar Travel Dryer",
-           category: "Hair Care",
-           price: 179.99,
-        },
-     
-        {
-           id: 7,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/11785994-1984985603593023.jpg",
-           name: " Philip B Icelandic Blonde Shampoo 32 fl oz/947ml",
-           category: "Hair Care",
-           price: 150.00,
-        },
-     
-        {
-           id: 8,
-           image_url: "https://static.thcdn.com/images/small/webp//productimg/original/13282502-3944891492637181.jpg",
-           name: " VIRTUE Healthy Scalp Trio",
-           category: "Hair Care",
-           price: 134.00,
-        },
-     
-        //makeup & cosmetics
-        {
-           id: 9,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/13937560-1154998594986205.jpg",
-           name: " Chantecaille Oil Free Balancing Moisturizer 50g",
-           category: "Makeup and Cosmetics",
-           price: 75.00,
-        },
-     
-        {
-           id: 10,
-           image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/11567391-1644522663544346.jpg",
-           name: " Dermablend Natural Finish Set (Various Shades)",
-           category: "Makeup and Cosmetics",
-           price: 64.00,
-        },
-     
-        {
-           id: 11,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/11348400-1224871519735586.jpg",
-           name: " Colorescience Natural Finish Pressed Foundation SPF20 (Various Shades)",
-           category: "Makeup and Cosmetics",
-           price: 59.00,
-        },
-     
-        {
-           id: 12,
-           image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11752239-8694597305372915.jpg",
-           name: "Koh Gen Do Maifanshi Moisture Fit Concealer 7g (Various Shades)",
-           category: "Makeup and Cosmetics",
-           price: 63.00,
-        },
-     
-        {
-           id: 13,
-           image_url: "https://static.thcdn.com/images/small/webp//productimg/original/11065895-5134853691666783.jpg",
-           name: "NUDESTIX Mascara - Black",
-           category: "Makeup and Cosmetics",
-           price: 28.00,
-        },
-     
-        {
-           id: 14,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/11378683-1514991779261836.jpg",
-           name: "Illamasqua Antimatter Lipstick (Various Shades)",
-           category: "Makeup and Cosmetics",
-           price: 11.00,
-        },
-     
-        {
-           id: 15,
-           image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11742829-1834590785162913.jpg",
-           name: "TONYMOLY Petit Bunny Gloss Bars (Option: 02 | Juicy Grape)",
-           category: "Makeup and Cosmetics",
-           price: 8.00,
-        },
-     
-        {
-           id: 16,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/12657561-1764964030368092.jpg",
-           name: "Stila Sketch and Sculpt Brow Pencil 4g (Various Shades)",
-           category: "Makeup and Cosmetics",
-           price: 20.00,
-        },
-     
-        //Bath & Body
-        {
-           id: 17,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/12507566-5965014350757716.jpg",
-           name: " Mama Mio Supersize Bundle",
-           category: "Bath & Body",
-           price: 91.00,
-        },
-     
-        {
-           id: 18,
-           image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11725414-2914576050842040.jpg",
-           name: "Elemis Love Yourself Bundle",
-           category: "Bath & Body",
-           price: 79.60,
-        },
-     
-        {
-           id: 19,
-           image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11691581-4674555612912272.jpg",
-           name: "Elemis Body Performance Bundle",
-           category: "Bath & Body",
-           price: 108.50,
-        },
-     
-        {
-           id: 20,
-           image_url: "https://static.thcdn.com/images/small/original//productimg/original/13030216-1194867945123005.jpg",
-           name: "Soon Skincare Clear Day Broad Spectrum SPF50 2 oz",
-           category: "Bath & Body",
-           price: 48.00,
-        }
+   //Skincare products
+   {
+    id: 1,
+    image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/11401314-1954461013882644.jpg",
+    name: "3LAB Anti-Ageing Cream 60ml",
+    category: "Skincare",
+    price: 750.00,
+ },
+
+ {
+    id: 2,
+    image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/12038576-2264648083937035.jpg",
+    name: "Omorovicza Best Sellers Set",
+    category: "Skincare",
+    price: 480.00,
+ },
+
+ {
+    id: 3,
+    image_url: "https://static.thcdn.com/images/small/webp//productimg/1600/1600/12538011-5734765050447840.jpg",
+    name: "Omorovicza Evening CBD Facial Set",
+    category: "Skincare",
+    price: 402.00,
+ },
+
+ {
+    id: 4,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13928075-1205002137017610.jpg",
+    name: " Zutta LED Mask - Sapphira",
+    category: "Skincare",
+    price: 399.00,
+ },
+
+ //Hair products
+ {
+    id: 5,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13280908-8024900284917718.jpg",
+    name: " Unplugged Styler - Cordless Flat Iron (Black)",
+    category: " Hair Care",
+    price: 299.00,
+ },
+
+ {
+    id: 6,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13554654-1074930173695055.jpg",
+    name: " T3 Afar Travel Dryer",
+    category: "Hair Care",
+    price: 179.99,
+ },
+
+ {
+    id: 7,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/11785994-1984985603593023.jpg",
+    name: " Philip B Icelandic Blonde Shampoo 32 fl oz/947ml",
+    category: "Hair Care",
+    price: 150.00,
+ },
+
+ {
+    id: 8,
+    image_url: "https://static.thcdn.com/images/small/webp//productimg/original/13282502-3944891492637181.jpg",
+    name: " VIRTUE Healthy Scalp Trio",
+    category: "Hair Care",
+    price: 134.00,
+ },
+
+ //makeup & cosmetics
+ {
+    id: 9,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13937560-1154998594986205.jpg",
+    name: " Chantecaille Oil Free Balancing Moisturizer 50g",
+    category: "Makeup and Cosmetics",
+    price: 75.00,
+ },
+
+ {
+    id: 10,
+    image_url: "https://static.thcdn.com/images/small/original/productimg/1600/1600/11567391-1644522663544346.jpg",
+    name: " Dermablend Natural Finish Set (Various Shades)",
+    category: "Makeup and Cosmetics",
+    price: 64.00,
+ },
+
+ {
+    id: 11,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/11348400-1224871519735586.jpg",
+    name: " Colorescience Natural Finish Pressed Foundation SPF20 (Various Shades)",
+    category: "Makeup and Cosmetics",
+    price: 59.00,
+ },
+
+ {
+    id: 12,
+    image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11752239-8694597305372915.jpg",
+    name: "Koh Gen Do Maifanshi Moisture Fit Concealer 7g (Various Shades)",
+    category: "Makeup and Cosmetics",
+    price: 63.00,
+ },
+
+ {
+    id: 13,
+    image_url: "https://static.thcdn.com/images/small/webp//productimg/original/11065895-5134853691666783.jpg",
+    name: "NUDESTIX Mascara - Black",
+    category: "Makeup and Cosmetics",
+    price: 28.00,
+ },
+
+ {
+    id: 14,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/11378683-1514991779261836.jpg",
+    name: "Illamasqua Antimatter Lipstick (Various Shades)",
+    category: "Makeup and Cosmetics",
+    price: 11.00,
+ },
+
+ {
+    id: 15,
+    image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11742829-1834590785162913.jpg",
+    name: "TONYMOLY Petit Bunny Gloss Bars (Option: 02 | Juicy Grape)",
+    category: "Makeup and Cosmetics",
+    price: 8.00,
+ },
+
+ {
+    id: 16,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/12657561-1764964030368092.jpg",
+    name: "Stila Sketch and Sculpt Brow Pencil 4g (Various Shades)",
+    category: "Makeup and Cosmetics",
+    price: 20.00,
+ },
+
+ //Bath & Body
+ {
+    id: 17,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/12507566-5965014350757716.jpg",
+    name: " Mama Mio Supersize Bundle",
+    category: "Bath & Body",
+    price: 91.00,
+ },
+
+ {
+    id: 18,
+    image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11725414-2914576050842040.jpg",
+    name: "Elemis Love Yourself Bundle",
+    category: "Bath & Body",
+    price: 79.60,
+ },
+
+ {
+    id: 19,
+    image_url: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11691581-4674555612912272.jpg",
+    name: "Elemis Body Performance Bundle",
+    category: "Bath & Body",
+    price: 108.50,
+ },
+
+ {
+    id: 20,
+    image_url: "https://static.thcdn.com/images/small/original//productimg/original/13030216-1194867945123005.jpg",
+    name: "Soon Skincare Clear Day Broad Spectrum SPF50 2 oz",
+    category: "Bath & Body",
+    price: 48.00,
+ }
 ];
 
 function addToCart(id){
